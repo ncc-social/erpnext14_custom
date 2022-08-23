9a4d85e37b2377fbaa3a9c8baeb36134589fea32
ncc-social <social@ncc.gov.gh>
2022-08-23 23:30:25 +0000

    Fixed taberror in leave_application.py

--- erpnext14_custom/server_scripts/leave_application.py
+++ erpnext14_custom/server_scripts/leave_application.py
@@ -30,9 +30,9 @@ from hrms.hr.utils import (
 
 def after_approval(self):
     if self.status == "Open":
-			frappe.throw(_("Only Leave Applications with status 'Approved' and 'Rejected' can be submitted"))
+	frappe.throw(_("Only Leave Applications with status 'Approved' and 'Rejected' can be submitted"))
 
-	self.validate_back_dated_application()
+    self.validate_back_dated_application()
 
 	# notify leave applier about approval
 	if frappe.db.get_single_value("HR Settings", "send_leave_notification"):
